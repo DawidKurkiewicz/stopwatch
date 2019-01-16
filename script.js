972b12cb1571a595af7f8eba977f235b5e149646
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-16 11:54:21 +0100

    uses toFixed"

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnTime = document.querySelector(".main");
 const btnReset = document.querySelector(".reset");
-const panel = document.querySelector("time div");
+const panel = document.querySelector(".time div");
 
 let time = 0;
 let active = false
@@ -9,11 +9,16 @@ const timer = () => {
     if (!active) {
         active = !active
         btnTime.textContent = "Pauza";
-    setInterval(start, 10);
+        setInterval(start, 10);
     } else {
         active = !active
         btnTime.textContent = "Start";
     }
+}
+
+const start = () => {
+    time++
+    panel.textContent = (time / 100).toFixed(2)
 }
 
 btnTime.addEventListener("click", timer);
